1aa2a16ef9e5bcee62e4add60518ef2cf159c5f4
Harshit Bagewadi <98148017+Harsh-Repository@users.noreply.github.com>
2023-07-12 14:25:10 +0530

    Delete server directory

--- server/src/data/models/CreateProjectModel.js
+++
@@ -1,6 +0,0 @@
-const { model } = require("mongoose");
-const { Project } = require("../schema/projectsSchema");
-
-const ProjectModel = new model(`ProjectModel`, Project);
-
-module.exports = { ProjectModel };
