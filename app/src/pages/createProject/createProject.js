5f2aef346681f4ac4cdb075ec07844d35659cae4
Harshit Bagewadi <98148017+Harsh-Repository@users.noreply.github.com>
2023-07-16 00:08:59 +0530

    Update createProject.js
    updated redirect path for project details submission button

--- app/src/pages/createProject/createProject.js
+++ app/src/pages/createProject/createProject.js
@@ -175,7 +175,7 @@ export const CreateProject = () => {
                     }
                   )
                   .then(alert(`Project created Successfully`))
-                  .then((res) => (window.location.href = "/projects"));
+                  .then((res) => (window.location.href = "https://aesthetic-frangipane-073db9.netlify.app/projects"));
                 setName("");
                 setEmail("");
                 setTitle("");
